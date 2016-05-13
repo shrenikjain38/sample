fa986645ff434891548575669e4438a40bcfe815
Shrenik <shrenik.jain@research.iiit.ac.in>
2016-05-13 18:04:40 +0530

    Script for VB-consul swarm cluster

---
+++ virtualbox_consul.sh
@@ -0,0 +1,16 @@
+unset http_proxy https_proxy all_proxy
+docker-machine create -d virtualbox manager
+docker-machine regenerate-certs manager -f
+docker-machine create -d virtualbox consul
+docker-machine regenerate-certs consul -f
+docker-machine create -d virtualbox agent1
+docker-machine regenerate-certs agent1 -f
+eval $(docker-machine env consul)
+docker run -d -p 8500:8500 --name=consul progrium/consul -server -bootstrap
+eval $(docker-machine env manager)
+docker run -d -p 3376:3376 -t -v /var/lib/boot2docker:/certs:ro swarm manage -H 0.0.0.0:3376 --tlsverify --tlscacert=/certs/ca.pem --tlscert=/certs/server.pem --tlskey=/certs/server-key.pem consul://$(docker-machine ip consul):8500
+eval $(docker-machine env agent1)
+docker run -d swarm join --addr=$(docker-machine ip agent1):2376 consul://$(docker-machine ip consul):8500
+#run after script has finished executing
+#DOCKER_HOST=$(docker-machine ip manager):3376
+#docker info
